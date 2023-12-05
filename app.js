e6f035ede2b3e044c83999d2d82ba3589ad4f633
adeniranadeyemo <niranadeniran1@gmail.com>
2023-12-05 22:15:25 +0100

    Added functionalities for each step

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
 const moreBtn = document.querySelector('.more');
 const guideOut = document.querySelector('.setup-guide-out');
 const guideIn = document.querySelector('.setup-guide-in');
+//
+const steps = document.querySelector('.steps');
+const allSteps = document.querySelectorAll('.step');
+
+guideOut.style.height = `8rem`;
+
+let guideInHeight;
+
+const stepReset = function () {
+  allSteps.forEach((step) => {
+    step.style.height = '40px';
+    step.classList.remove('opened-step');
+  });
+};
 
 moreBtn.addEventListener('click', () => {
   moreBtn.classList.toggle('rotate');
 
-  const guideOutHeight = guideOut.getBoundingClientRect().height;
-  const guideInHeight = guideIn.getBoundingClientRect().height;
+  stepReset();
 
-  if (guideOutHeight === 136) {
-    guideOut.style.height = `${guideInHeight}px`;
-  }
-
-  if (guideOutHeight === guideInHeight) {
-    guideOut.style.height = `136px`;
-    console.log(3);
+  guideOut.classList.toggle('show');
+  if (guideOut.classList.contains('show')) {
+    guideOut.style.overflow = 'visible';
+  } else {
+    guideOut.style.overflow = 'hidden';
   }
 });
+
+const openClose = function (e) {
+  const stepOut = e.target.closest('.step');
+
+  if (stepOut) {
+    // Close all open steps
+    stepReset();
+
+    stepOut.classList.add('opened-step');
+
+    // Open the clicked step
+    const stepOutHeight = stepOut.getBoundingClientRect().height;
+
+    const stepIn = stepOut.querySelector('.step-inner');
+    const stepInHeight = stepIn.getBoundingClientRect().height;
+
+    if (stepOutHeight) {
+      stepOut.style.height = `${stepInHeight}px`;
+    }
+
+    if (stepOutHeight === stepInHeight) {
+      stepOut.style.height = '40px';
+    }
+  }
+};
+
+steps.addEventListener('click', openClose);
